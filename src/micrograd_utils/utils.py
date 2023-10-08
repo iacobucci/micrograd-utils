80b8f68abdfbee3b43b84ca7516593bc2c540994
iacobucci <iacobuccivalerio@gmail.com>
2023-10-08 23:57:19 +0200

    update to version version = "0.0.4"

--- src/micrograd_utils/utils.py
+++ src/micrograd_utils/utils.py
@@ -3,25 +3,29 @@
 from micrograd.engine import *
 from micrograd.nn import *
 
-def dump_parameters(m, filename="params.txt"):
-	nout = []
-	for l in range(len(m.layers)):
-		if l == 0:
-			nin = len(m.layers[l].neurons[0].w)
-		nout.append(len(m.layers[l].neurons))
-			
-	with open(filename, "w") as f:
-		f.write("MLP(" + str(nin) + ", " + str(nout) + ")")
-		f.write("\n")
-		f.write(str(['Value(data=' + str(v.data) + ')' for v in m.parameters()]).replace("'",""))
 
-def load_parameters(filename="params.txt"):
-	with open(filename, "r") as f:
-		lines = f.readlines()
-		model = eval(lines[0])
-		params = eval(lines[1])
-		for p, q in zip(model.parameters(), params):
-			p.data = q.data
-	return model
+def dump(m, filename="params.py"):
+    nout = []
+    for l in range(len(m.layers)):
+        if l == 0:
+            nin = len(m.layers[l].neurons[0].w)
+        nout.append(len(m.layers[l].neurons))
 
+    with open(filename, "w") as f:
+        f.write("from micrograd.engine import *")
+        f.write("\n")
+        f.write("from micrograd.nn import *")
+        f.write("\n")
+        f.write("\n")
+        f.write("parameters = ")
+        f.write(
+            str(['Value(data=' + str(v.data) + ')' for v in m.parameters()]).replace("'", ""))
+        f.write("\n")
+        f.write("\n")
+        f.write("def model():\n")
+        f.write("\tmodel = MLP(" + str(nin) + ", " + str(nout) + ")\n")
+        f.write("\tfor p, q in zip(model.parameters(), parameters):\n")
+        f.write("\t\tp.data = q.data\n")
+        f.write("\treturn model\n")
+        f.write("\n")
 
---
+++ tests/micrograd_utils/dump_and_load.py
@@ -0,0 +1,58 @@
+import pytest
+import sys
+import os
+
+# Ottieni il percorso completo alla directory src/micrograd_utils
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/micrograd_utils'))
+
+# Aggiungi src al percorso di ricerca dei moduli
+sys.path.insert(0, src_path)
+
+import utils
+
+from micrograd.engine import *
+from micrograd.nn import *
+
+def dump():
+    d = {
+        (0,): 0,
+        (1,): 1,
+    }
+
+    dataset = [list(k) for k, v in d.items()]
+    desired = [v for k, v in d.items()]
+
+    m = MLP(1, [16, 16, 1])
+
+    for k in range(100):
+        # forward pass with mean squared error loss
+        ypred = [m(x) for x in dataset]
+        loss = sum([(yout - ygt)**2 for ygt, yout in zip(desired, ypred)])
+        # zero gradients
+        m.zero_grad()
+        # backward pass
+        loss.backward()
+        # update weights
+        for p in m.parameters():
+            p.data -= p.grad * 0.05
+
+        print(k, loss)
+
+    utils.dump(m)
+
+def load():
+    import params
+
+    m = params.model()
+
+    new_data = [
+        [0],
+        [1],
+    ]
+
+    for x in new_data:
+        result = m(x)
+        print("{", x[0], ":", result.data, "}")
+
+        assert abs(result.data - x[0]) < 0.01
+
